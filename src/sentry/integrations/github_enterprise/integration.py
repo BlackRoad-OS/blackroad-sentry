998a559ffb215a7bacd289e7e859f9e61123a67b
Raj Joshi <rajjoshi.0222@gmail.com>
2026-01-01 19:19:07 -0500

    :bug: fix: pr feedback

--- src/sentry/integrations/github_enterprise/integration.py
+++ src/sentry/integrations/github_enterprise/integration.py
@@ -301,7 +301,9 @@ class GitHubEnterpriseIntegration(
         """
         config: list[dict[str, Any]] = []
 
-        if features.has("organizations:integrations-github-project-management", self.organization):
+        if features.has(
+            "organizations:integrations-github_enterprise-project-management", self.organization
+        ):
             config.extend(
                 [
                     {
